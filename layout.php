b997316c3324c17f5909822ad8d220221310ac40
VitalyMayer <vitalymayer34@gmail.com>
2022-09-02 17:22:44 +0300

    Breaking the layout into components

---
+++ layout.php
@@ -0,0 +1,13 @@
+<?php
+require_once(ROOT . '/header.php');
+require_once(ROOT . '/navigation.php');
+?>
+
+<main>
+    <article>
+        <h2>Post 1</h2>
+        <p></p>
+    </article>
+</main>
+
+<?php require_once(ROOT . '/footer.php'); ?>
