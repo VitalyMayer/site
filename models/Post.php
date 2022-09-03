87a64894085cfb3e4c7e5657d32db23457c2faaf
VitalyMayer <vitalymayer34@gmail.com>
2022-09-03 11:27:03 +0300

    Database addition

--- models/Post.php
+++ models/Post.php
@@ -1,18 +1,38 @@
 <?php
 
-class Post implements Model
-{
-    public static function getPostsList()
-    {
-        include_once 'posts.php';
-        
-        return $posts;
+class Post implements Model {
+
+    public static function getPostsList() {
+        $db = Db::getConnection();
+
+        $postsList = array();
+
+        $result = $db->query('SELECT id, title, content FROM posts ');
+
+        $i = 0;
+
+        while ($row = $result->fetch()) {
+            $postsList[$i]['id'] = $row['id'];
+            $postsList[$i]['title'] = $row['title'];
+            $postsList[$i]['content'] = $row['content'];
+            $i++;
+        }
+
+        return $postsList;
     }
-    
-    public static function getPostItemById($id)
-    {
-         include_once 'posts.php';
-         
-         return $posts[$id];
+
+    public static function getPostItemById($id) {
+        $id = intval($id);
+
+        if ($id) {
+            $db = Db::getConnection();
+
+            $result = $db->query('SELECT * FROM posts WHERE id=' . $id);
+
+            $post = $result->fetch();
+
+            return $post;
+        }
     }
+
 }
